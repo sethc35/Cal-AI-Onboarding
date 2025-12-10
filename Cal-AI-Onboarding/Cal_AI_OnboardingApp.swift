7e53d802d7db341f8054ac6e3252052093fccad4
Warrior-Wombat <sethchangcom@gmail.com>
2025-12-09 22:51:05 -0800

    Initial Commit

---
+++ Cal-AI-Onboarding/Cal_AI_OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cal_AI_OnboardingApp.swift
+//  Cal-AI-Onboarding
+//
+//  Created by Seth Chang on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Cal_AI_OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cal-AI-Onboarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cal-AI-Onboarding
+//
+//  Created by Seth Chang on 12/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
